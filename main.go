65b58072bbf0d45b68f7097bbdda6be90037e082
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-05-15 15:29:22 +0900

    1-2章 ハンドラをいくつか実装した

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,7 +13,39 @@ func main() {
 		io.WriteString(w, "Hello, World!\n")
 	}
 
-	http.HandleFunc("/", hellHandler)
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("Article No:%d\n", articleID)
+		io.WriteString(w, resString)
+	}
+
+	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
+	http.HandleFunc("/hello", hellHandler)
+
+	http.HandleFunc("/article", postArticleHandler)
+
+	http.HandleFunc("/article/list", articleListHandler)
+
+	http.HandleFunc("/article/1", articleDetailHandler)
+
+	http.HandleFunc("/article/nice", postNiceHandler)
+
+	http.HandleFunc("/comment", postCommentHandler)
 
 	log.Println("server start at port 8080")
 
