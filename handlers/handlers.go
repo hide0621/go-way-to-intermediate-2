5edd3135a1915ecadebe4ac09ee923a1774787b2
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-05-26 14:23:02 +0900

    2-5章 HTTPリクエストからリクエストボディを読み取る部分の実装

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,6 +17,19 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content\n", http.StatusBadRequest)
+	}
+	reqBodyBuffer := make([]byte, length)
+
+	if _, err := req.Body.Read(reqBodyBuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "fail to get request body\n", http.StatusInternalServerError)
+		return
+	}
+	defer req.Body.Close()
+
 	article := models.Article1
 	jsonData, err := json.Marshal(article)
 	if err != nil {
